8bfd1c4e2cbaf73a648a94660bc7869d901fac1f
Daniel Cruz <dsestupinan71@misena.edu.co>
2021-04-19 21:34:20 -0500

    Update src's in index.html

---
+++ docs/precache-manifest.9442e50ca8422c907af73ab3b6eb749b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670c628ddf03d43579838da84288d10a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c30bd75d08b0484ed79c",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "64c47abffa510b2f7cb5",
+    "url": "/static/css/main.e9fd27c4.chunk.css"
+  },
+  {
+    "revision": "c30bd75d08b0484ed79c",
+    "url": "/static/js/2.ab67e774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab67e774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e1bbbaebbcdf1d9259",
+    "url": "/static/js/3.3e6078bb.chunk.js"
+  },
+  {
+    "revision": "64c47abffa510b2f7cb5",
+    "url": "/static/js/main.fbee300d.chunk.js"
+  },
+  {
+    "revision": "735e9acea98e6cb686df",
+    "url": "/static/js/runtime-main.c910cc03.js"
+  }
+]);
